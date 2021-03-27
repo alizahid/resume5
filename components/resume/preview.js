f49a167e8e3a58217afd594ed267097bcdc228a6
Ali Zahid <ali.zahid@live.com>
2021-03-27 19:58:21 +0500

    Styles updated

--- components/resume/preview.js
+++ components/resume/preview.js
@@ -4,8 +4,15 @@ import React from 'react'
 
 export const ResumePreview = ({ className, data, theme }) => (
   <div className={clsx('m-4', className)}>
-    <div>{theme}</div>
-    <pre className="mt-4">{JSON.stringify(data, null, 2)}</pre>
+    <div className="flex items-center">
+      <span className="font-medium">Theme</span>
+      <span className="ml-4 bg-teal-500 text-white rounded-xl p-2">
+        {theme}
+      </span>
+    </div>
+    <pre className="text-sm leading-relaxed mt-4">
+      {JSON.stringify(data, null, 2)}
+    </pre>
   </div>
 )
 
